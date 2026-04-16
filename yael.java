941af1d92f4f97601613fc02276d9db322814159
yaelmosh <yaelmo30@gmail.com>
2026-04-16 17:50:16 +0300

    yael.java changes

--- yael.java
+++ yael.java
@@ -1,7 +1,7 @@
 public class Yael {
     public static void main(String[] args) {
         for (int i=1; i<=5; i++) {
-            System.out.println("Yael Moshkovich" + i);
+            System.out.println("Yael Moshkovich");
         }
     }
 }
