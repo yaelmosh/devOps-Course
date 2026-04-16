fc636d5f2387df3331eb675f4d3ea36e16a81f67
yaelmosh <yaelmo30@gmail.com>
2026-04-16 17:56:32 +0300

    yael.java changes

--- yael.java
+++ yael.java
@@ -1,7 +1,7 @@
 public class Yael {
     public static void main(String[] args) {
         for (int i=1; i<=5; i++) {
-            System.out.println("Yael Moshkovich");
+            System.out.println("Jenkins is building Yael ..." + i);
         }
     }
 }
